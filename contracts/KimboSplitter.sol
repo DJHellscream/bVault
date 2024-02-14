83f9a7215cd875455c9fb5d2c85351d3cbcf2aa5
bifkn <tmcollins4@gmail.com>
2024-02-14 12:45:12 -0500

    added thirdweb-dev dependencies and inherit from Split.sol

---
+++ contracts/KimboSplitter.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v4.8.0) (finance/PaymentSplitter.sol)
+
+pragma solidity ^0.8.0;
+
+import "@thirdweb-dev/contracts/prebuilts/split/Split.sol";
+
+contract PaymentSplitter is Split {
+
+}
